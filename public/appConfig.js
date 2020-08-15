49522f76c0a144264f243567f55a3b585d41fa8d
GrahamsRepos <54447363+GrahamsRepos@users.noreply.github.com>
2020-08-15 18:36:29 +0200

    Update appConfig.js

--- public/appConfig.js
+++ public/appConfig.js
@@ -1,5 +1,5 @@
 // your account sid
-var accountSid = 'ACf20da6bdc72bb2e4f6da9ba880c58ccd';
+var accountSid = '';
 
 // set to /plugins.json for local dev
 // set to /plugins.local.build.json for testing your build
